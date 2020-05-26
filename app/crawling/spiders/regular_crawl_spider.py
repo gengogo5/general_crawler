fed04f60e6e8ecac68a7b57fcc03d3a7e53cdd0d
gengogo5 <gen0908@gmail.com>
2020-05-26 20:16:12 +0900

    #9 indexパターンが無くても動くようにする

--- app/crawling/spiders/regular_crawl_spider.py
+++ app/crawling/spiders/regular_crawl_spider.py
@@ -19,16 +19,17 @@ class RegularCrawlSpider(CrawlSpider):
         # 引数から各種設定を取得
         params = json.loads(self.payload)
         self.start_urls = params['start_urls'] # 必須
-        index_patterns  = params['index_patterns'] # 任意
-        allow_patterns  = params['article_patterns'] # 任意
-        deny_patterns   = params['except_article_patterns'] # 任意
+        index_patterns  = params.get('index_patterns') # 任意
+        allow_patterns  = params.get('article_patterns') # 任意
+        deny_patterns   = params.get('except_article_patterns') # 任意
 
         # ルール設定
         # TODO: raw_ruleは最初からrules変数使っていいかもしれない(項目数が可変か検討)
         # TODO: 正規表現を事前サニタイズするかどうか(検証済を受け入れる前提でも可)
         # TODO: followの制御フラグ
         raw_rule = []
-        raw_rule.append(Rule(LinkExtractor(allow=tuple(index_patterns))))
+        if index_patterns:
+            raw_rule.append(Rule(LinkExtractor(allow=tuple(index_patterns))))
         raw_rule.append(Rule(LinkExtractor(allow=tuple(allow_patterns), deny=tuple(deny_patterns)), follow=False, callback='parse_item'))
 
         self.rules = tuple(raw_rule)
