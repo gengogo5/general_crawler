1ad65d15af528e4c64358edea32fed1a3f351de4
gengogo5 <gen0908@gmail.com>
2020-05-26 23:30:28 +0900

    #11 除外記事パターンを追加

--- app/crawling/spiders/rss_crawl_spider.py
+++ app/crawling/spiders/rss_crawl_spider.py
@@ -2,11 +2,13 @@
 import scrapy
 import logging
 import json
+import re
 from scrapy.spiders import XMLFeedSpider
 from crawling.article_archives import ArticleArchives
 
 class RSSCrawlSpider(XMLFeedSpider):
     name = 'rss_crawl'
+    except_regexps = []
 
     custom_settings = {
         'DUPEFILTER_CLASS': 'crawling.dupefilter.ArticleArchiveDupeFilter'
@@ -19,11 +21,20 @@ class RSSCrawlSpider(XMLFeedSpider):
         self.start_urls  = params['rss_urls']  # 必須
         self.itertag     = params['tag_name']  # 必須
         self.link_node   = params['link_node_name'] # 必須
+        for p in params.get('except_article_patterns', []): # 任意
+            self.except_regexps.append(re.compile(p))
 
     # 繰り返しのタグ見つけたら、linkノードからurlを取得する
     def parse_node(self, response, node):
         url = node.xpath(f'./{self.link_node}/text()').extract()[0]
-        return scrapy.Request(response.urljoin(url), self.parse_item)
+        joined_url = response.urljoin(url)
+
+        # 除外記事
+        for r in self.except_regexps:
+            if r.search(joined_url):
+                logging.debug(f'excepted page [{joined_url}]')
+                return
+        return scrapy.Request(joined_url, self.parse_item)
         
     def parse_item(self, response):
         item = ArticleArchives()
