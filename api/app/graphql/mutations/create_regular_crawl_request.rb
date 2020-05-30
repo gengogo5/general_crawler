cefb7c042b973e7e20dbf83782c7a7640a82702a
gengogo5 <gen0908@gmail.com>
2020-05-30 20:29:00 +0900

    #21 記事リスト版の登録API追加

--- api/app/graphql/mutations/create_regular_crawl_request.rb
+++ api/app/graphql/mutations/create_regular_crawl_request.rb
@@ -1,14 +1,36 @@
 module Mutations
   class CreateRegularCrawlRequest < BaseMutation
-    # TODO: define return fields
-    # field :post, Types::PostType, null: false
+    graphql_name 'CreateRegularCrawlRequest'
+    field :crawl_request, Types::CrawlRequestType, null: true
+    field :result, Boolean, null: true
 
-    # TODO: define arguments
-    # argument :name, String, required: true
+    argument :job_type, String, required: true
+    argument :start_urls, [String], required: true
+    argument :index_patterns, [String], required: false
+    argument :article_patterns, [String], required: false
+    argument :except_article_patterns, [String], required: false
+    argument :should_follow, Int, required: false
+    argument :schedule_type, String, required: false
+    argument :interval_hours, Int, required: false
 
-    # TODO: define resolve method
-    # def resolve(name:)
-    #   { post: ... }
-    # end
+    def resolve(**args)
+      request = CrawlRequest.create(
+        job_type: args[:job_type],
+        rules: JSON.generate(
+          { :start_urls => args[:start_urls],
+            :index_patterns => args[:index_patterns],
+            :article_patterns => args[:article_patterns],
+            :except_article_patterns => args[:except_article_patterns],
+            :should_follow => args[:should_follow],
+          }
+        ),
+        schedule_type: args[:schedule_type],
+        interval_hours: args[:interval_hours]       
+      )
+      {
+        crawl_request: request,
+        result: request.errors.blank?
+      }
+    end
   end
 end
