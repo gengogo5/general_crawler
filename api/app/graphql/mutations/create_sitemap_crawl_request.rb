6c2ded447ac1a06b3b7f41364fc1099f44b110e3
gengogo5 <gen0908@gmail.com>
2020-05-30 18:58:33 +0900

    #21 サイトマップ版の登録API追加

--- api/app/graphql/mutations/create_sitemap_crawl_request.rb
+++ api/app/graphql/mutations/create_sitemap_crawl_request.rb
@@ -1,14 +1,32 @@
 module Mutations
   class CreateSitemapCrawlRequest < BaseMutation
-    # TODO: define return fields
-    # field :post, Types::PostType, null: false
+    graphql_name 'CreateSitemapCrawlRequest'
+    field :crawl_request, Types::CrawlRequestType, null: true
+    field :result, Boolean, null: true
 
-    # TODO: define arguments
-    # argument :name, String, required: true
+    argument :job_type, String, required: true
+    argument :sitemap_url, String, required: true
+    argument :sitemap_patterns, [String], required: false
+    argument :except_article_patterns, [String], required: false
+    argument :schedule_type, String, required: false
+    argument :interval_hours, Int, required: false
 
-    # TODO: define resolve method
-    # def resolve(name:)
-    #   { post: ... }
-    # end
+    def resolve(**args)
+      request = CrawlRequest.create(
+        job_type: args[:job_type],
+        rules: JSON.generate(
+          { :sitemap_url => args[:sitemap_url],
+            :sitemap_patterns => args[:sitemap_patterns],
+            :except_article_patterns => args[:except_article_patterns]
+          }
+        ),
+        schedule_type: args[:schedule_type],
+        interval_hours: args[:interval_hours]       
+      )
+      {
+        crawl_request: request,
+        result: request.errors.blank?
+      }
+    end
   end
 end
