54555e9f8f23efae95c99e97f197cc9a983ded58
gengogo5 <gen0908@gmail.com>
2020-05-30 12:29:20 +0900

    #21 Mutation定義追加

--- api/app/graphql/types/mutation_type.rb
+++ api/app/graphql/types/mutation_type.rb
@@ -1,5 +1,8 @@
 module Types
   class MutationType < Types::BaseObject
+    field :delete_crawl_request, mutation: Mutations::DeleteCrawlRequest
+    field :update_crawl_request, mutation: Mutations::UpdateCrawlRequest
+    field :create_crawl_request, mutation: Mutations::CreateCrawlRequest
     # TODO: remove me
     field :test_field, String, null: false,
       description: "An example field added by the generator"
