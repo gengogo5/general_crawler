e451df18c916fa602a9f6993992bb77e8b3e6bb3
gengogo5 <gen0908@gmail.com>
2020-05-31 15:30:37 +0900

    #23 dryrunの時はDBの設定に関わらず即時実行にする

--- api/app/graphql/mutations/schedule_job.rb
+++ api/app/graphql/mutations/schedule_job.rb
@@ -12,12 +12,16 @@ module Mutations
       job = CrawlRequest.find(args[:request_id])
 
       client = HTTPClient.new
-
       url = 'http://scrapy:7654/schedule-job.json'
+
+      # dryrunの時は即時実行にする
+      is_dryrun = args[:is_dryrun]
+      schedule_type = is_dryrun ? 'now' : job.schedule_type
+
       body = {'project'=> 'crawling',
               'spider' => "#{job.job_type}_crawl",
-              'when' => "#{job.schedule_type}",
-              'payload' => "{\"req_id\": #{job.id}, \"is_dryrun\": #{args[:is_dryrun] ? 1 : 0}}"}
+              'when' => "#{schedule_type}",
+              'payload' => "{\"req_id\": #{job.id}, \"is_dryrun\": #{is_dryrun}}"}
       response = client.post(url, body)
 
       {
