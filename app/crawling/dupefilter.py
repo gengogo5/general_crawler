1c67d58e502870f18d4adda303927d5574e07be0
gengogo5 <gen0908@gmail.com>
2020-06-01 09:25:23 +0900

    #7 spiderが持つurl置換パターンを使って重複確認する

--- app/crawling/dupefilter.py
+++ app/crawling/dupefilter.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 from scrapy.dupefilters import RFPDupeFilter
 from scrapy.utils.project import get_project_settings
+import re
 import textwrap, MySQLdb
 
 class ArticleArchiveDupeFilter(RFPDupeFilter):
+
+    # inject from CustomScheduler
+    spider = None
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -14,13 +18,19 @@ class ArticleArchiveDupeFilter(RFPDupeFilter):
 
     # DBに重複レコードがあったら既知と判断してDROP
     def request_seen(self, request):
+        request_url = request.url
+        if self.spider.url_replace_pattern:
+            request_url = re.sub(self.spider.url_replace_pattern, \
+                                 self.spider.replace_new_string, \
+                                 request_url)
+
         sql = textwrap.dedent('''\
         SELECT id
           FROM article_archives
          WHERE url = %(url)s
         ''')
         c = self.conn.cursor()
-        c.execute(sql, {'url': request.url})
+        c.execute(sql, {'url': request_url})
         return c.rowcount > 0
 
     # 後処理をオーバライド
