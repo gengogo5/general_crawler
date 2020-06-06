a0ee20719a6e14851bc9c6a90cb439291f3f5f8f
gengogo5 <gen0908@gmail.com>
2020-06-06 16:17:00 +0900

    #41 再クロール登録mutation追加

--- api/app/graphql/types/mutation_type.rb
+++ api/app/graphql/types/mutation_type.rb
@@ -3,16 +3,11 @@ module Types
     field :create_regular_crawl_request, mutation: Mutations::CreateRegularCrawlRequest
     field :create_sitemap_crawl_request, mutation: Mutations::CreateSitemapCrawlRequest
     field :create_rss_crawl_request, mutation: Mutations::CreateRssCrawlRequest
+    field :create_revisit_crawl_request, mutation: Mutations::CreateRevisitCrawlRequest
     # TODO: 更新と削除
     field :delete_crawl_request, mutation: Mutations::DeleteCrawlRequest
     field :update_crawl_request, mutation: Mutations::UpdateCrawlRequest
     field :schedule_job, mutation: Mutations::ScheduleJob
     field :cancel_job, mutation: Mutations::CancelJob
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World"
-    end
   end
 end
