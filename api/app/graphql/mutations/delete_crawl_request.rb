7259af8778fd55c9dee5113433264ae536b73cbc
gengogo5 <gen0908@gmail.com>
2020-06-07 09:26:41 +0900

    #21 クロール要求削除APIを追加

--- api/app/graphql/mutations/delete_crawl_request.rb
+++ api/app/graphql/mutations/delete_crawl_request.rb
@@ -1,14 +1,24 @@
 module Mutations
   class DeleteCrawlRequest < BaseMutation
-    # TODO: define return fields
-    # field :post, Types::PostType, null: false
+    graphql_name 'DeleteCrawlRequest'
+    field :crawl_request, Types::CrawlRequestType, null: true
+    field :result, Boolean, null: true
 
-    # TODO: define arguments
-    # argument :name, String, required: true
+    argument :request_id, ID, required: true
 
-    # TODO: define resolve method
-    # def resolve(name:)
-    #   { post: ... }
-    # end
+    def resolve(**args)
+      job = CrawlRequest.find(args[:request_id])
+
+      # スケジュール済みの場合は停止してから削除
+      if job.job_id.present?
+        client = HTTPClient.new
+        url = 'http://scrapy:7654/cancel-job.json'
+        client.post(url, {id: job.job_id})
+      end
+
+      {
+        result: job.destroy
+      }
+    end
   end
 end
--- api/app/graphql/types/mutation_type.rb
+++ api/app/graphql/types/mutation_type.rb
@@ -4,8 +4,8 @@ module Types
     field :create_sitemap_crawl_request, mutation: Mutations::CreateSitemapCrawlRequest
     field :create_rss_crawl_request, mutation: Mutations::CreateRssCrawlRequest
     field :create_revisit_crawl_request, mutation: Mutations::CreateRevisitCrawlRequest
-    # TODO: 更新と削除
     field :delete_crawl_request, mutation: Mutations::DeleteCrawlRequest
+    # TODO: 更新と削除
     field :update_crawl_request, mutation: Mutations::UpdateCrawlRequest
     field :schedule_job, mutation: Mutations::ScheduleJob
     field :cancel_job, mutation: Mutations::CancelJob
