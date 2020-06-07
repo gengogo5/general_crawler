514cea187397fa505f817839a0deaedc15b2e1b8
gengogo5 <gen0908@gmail.com>
2020-06-07 09:38:26 +0900

    #21 クロール要求更新APIは保留

--- api/app/graphql/mutations/delete_crawl_request.rb
+++ api/app/graphql/mutations/delete_crawl_request.rb
@@ -10,12 +10,12 @@ module Mutations
       job = CrawlRequest.find(args[:request_id])
 
       # スケジュール済みの場合は停止してから削除
+      # 若干やりすぎ感はある。スケジュール済みはエラーで返しても良いかもしれない
       if job.job_id.present?
         client = HTTPClient.new
         url = 'http://scrapy:7654/cancel-job.json'
         client.post(url, {id: job.job_id})
       end
-
       {
         result: job.destroy
       }
