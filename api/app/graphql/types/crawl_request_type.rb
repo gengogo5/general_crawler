3c614767adc287674a7f238349cb27713e5fea53
gengogo5 <gen0908@gmail.com>
2020-06-09 22:20:51 +0900

    #49 クエリ用のモデルを修正

--- api/app/graphql/types/crawl_request_type.rb
+++ api/app/graphql/types/crawl_request_type.rb
@@ -4,9 +4,23 @@ module Types
     field :job_id, String, null: true
     field :job_type, String, null: false
     field :rules, String, null: true
-    field :start_urls, String, null: true
-    field :tag_name, String, null: true
-    field :link_node_name, String, null: true
+    field :start_urls, [String], null: true
+    field :index_patterns, [String], null: true
+    field :article_patterns, [String], null: true
+    field :except_article_patterns, [String], null: true
+    field :should_follow, Integer, null: true
+    field :url_replace_pattern, String, null: true
+    field :replace_new_string, String, null: true
+    field :user_agent, String, null: true
+    field :login_url,  String, null: true
+    field :login_name, String, null: true
+    field :login_password, String, null: true
+    field :login_name_attr, String, null: true
+    field :login_pass_attr, String, null: true
+    field :tag_name,        String, null: true
+    field :link_node_name,  String, null: true
+    field :sitemap_patterns, [String], null: true
+    field :url_prefix, String, null: true
     field :is_dryrun, Boolean, null: true
     field :schedule_type, String, null: true
     field :interval_hours, Integer, null: true
--- api/app/models/crawl_request.rb
+++ api/app/models/crawl_request.rb
@@ -1,27 +1,46 @@
 class CrawlRequest < ApplicationRecord
 
   after_find :parse_rules
-  attr_accessor :tag_name,
-                :link_node_name,
-                :except_article_patterns,
-                :start_urls,
+  attr_accessor :start_urls,
                 :index_patterns,
                 :article_patterns,
-                :sitemap_url,
-                :sitemap_patterns
+                :except_article_patterns,
+                :should_follow,
+                :url_replace_pattern,
+                :replace_new_string,
+                :user_agent,
+                :login_url,
+                :login_name,
+                :login_password,
+                :login_name_attr,
+                :login_pass_attr,
+                :tag_name,
+                :link_node_name,
+                :sitemap_patterns,
+                :url_prefix
 
   private
 
   # JSONのクロール設定をparseする
   # TODO: パラメータが確定したらクエリ用に全部書く
   def parse_rules
-    self.tag_name = {:tag_name => self[:rules]['tag_name']}
-    self.link_node_name = {:link_node_name => self[:rules]['link_node_name']}
-    self.except_article_patterns = {:except_article_patterns => self[:rules]['except_article_patterns']}
-    self.start_urls = {:start_urls => self[:rules]['start_urls']}
-    self.index_patterns = {:index_patterns => self[:rules]['index_patterns']}
-    self.article_patterns = {:article_patterns => self[:rules]['article_patterns']}
-    self.sitemap_url = {:sitemap_url => self[:rules]['sitemap_url']}
-    self.sitemap_patterns = {:sitemap_patterns => self[:rules]['sitemap_patterns']}
+    rulesj = JSON.parse(self[:rules])
+    self.start_urls              = rulesj['start_urls']
+    self.index_patterns          = rulesj['index_patterns']
+    self.article_patterns        = rulesj['article_patterns']
+    self.except_article_patterns = rulesj['except_article_patterns']
+    self.should_follow           = rulesj['should_follow']
+    self.url_replace_pattern     = rulesj['url_replace_pattern']
+    self.replace_new_string      = rulesj['replace_new_string']
+    self.user_agent              = rulesj['user_agent']
+    self.login_url               = rulesj['login_url']
+    self.login_name              = rulesj['login_name']
+    self.login_password          = rulesj['login_password']
+    self.login_name_attr         = rulesj['login_name_attr']
+    self.login_pass_attr         = rulesj['login_pass_attr']
+    self.tag_name                = rulesj['tag_name']
+    self.link_node_name          = rulesj['link_node_name']
+    self.sitemap_patterns        = rulesj['sitemap_patterns']
+    self.url_prefix              = rulesj['url_prefix']
   end
 end
